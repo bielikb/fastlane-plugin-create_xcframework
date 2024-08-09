d7225d6d9d28c55478413c74746a1d17ad1396e3
yogesh khandelwal <er.yogeshkhandelwal@gmail.com>
2024-08-09 13:58:09 +0530

    Adding zip,delete and ignore some module
    Adding some more functionalities
    Zipping of xcframework
    Deletion of xcframework
    Ignore module reference to solve https://developer.apple.com/forums/thread/123253
    Signing of the xcframework
    From a single archive, creating multiple xcframework as archive has multiple frameworks included.

--- lib/fastlane/plugin/create_xcframework/actions/create_xcframework_action.rb
+++ lib/fastlane/plugin/create_xcframework/actions/create_xcframework_action.rb
@@ -26,6 +26,10 @@ module Fastlane
           end
 
           create_xcframework(params)
+          remove_module_reference(params)
+          sign_xcframework(params)
+          zip_xcframework(params)
+          delete_xcframework(params)
 
           copy_dSYMs(params)
 
@@ -33,19 +37,28 @@ module Fastlane
 
           clean(params)
 
-          provide_shared_values
+          
+
+          provide_shared_values(params)
         else
           UI.important('xcframework can be produced only using Xcode 11 and above')
         end
       end
 
-      def self.provide_shared_values
-        Actions.lane_context[SharedValues::XCFRAMEWORK_OUTPUT_PATH] = File.expand_path(@xchelper.xcframework_path)
-        ENV[SharedValues::XCFRAMEWORK_OUTPUT_PATH.to_s] = File.expand_path(@xchelper.xcframework_path)
-        Actions.lane_context[SharedValues::XCFRAMEWORK_DSYM_OUTPUT_PATH] = File.expand_path(@xchelper.xcframework_dSYMs_path)
-        ENV[SharedValues::XCFRAMEWORK_DSYM_OUTPUT_PATH.to_s] = File.expand_path(@xchelper.xcframework_dSYMs_path)
-        Actions.lane_context[SharedValues::XCFRAMEWORK_BCSYMBOLMAPS_OUTPUT_PATH] = File.expand_path(@xchelper.xcframework_BCSymbolMaps_path)
-        ENV[SharedValues::XCFRAMEWORK_BCSYMBOLMAPS_OUTPUT_PATH.to_s] = File.expand_path(@xchelper.xcframework_BCSymbolMaps_path)
+      def self.provide_shared_values(params)
+        frameworks = params[:frameworks] || [nil]
+        frameworks.each do |framework|
+          xcframework_path = framework ? @xchelper.get_xcframework_path(framework) : @xchelper.xcframework_path
+          dsyms_path = framework ? @xchelper.framework_dSYMs_path(framework) : @xchelper.xcframework_dSYMs_path
+          bcsymbolmaps_path = framework ? @xchelper.framework_BCSymbolMaps_path(framework) : @xchelper.xcframework_BCSymbolMaps_path
+      
+          Actions.lane_context[SharedValues::XCFRAMEWORK_OUTPUT_PATH] = File.expand_path(xcframework_path)
+          ENV[SharedValues::XCFRAMEWORK_OUTPUT_PATH.to_s] = File.expand_path(xcframework_path)
+          Actions.lane_context[SharedValues::XCFRAMEWORK_DSYM_OUTPUT_PATH] = File.expand_path(dsyms_path)
+          ENV[SharedValues::XCFRAMEWORK_DSYM_OUTPUT_PATH.to_s] = File.expand_path(dsyms_path)
+          Actions.lane_context[SharedValues::XCFRAMEWORK_BCSYMBOLMAPS_OUTPUT_PATH] = File.expand_path(bcsymbolmaps_path)
+          ENV[SharedValues::XCFRAMEWORK_BCSYMBOLMAPS_OUTPUT_PATH.to_s] = File.expand_path(bcsymbolmaps_path)
+        end
       end
 
       def self.clean(params)
@@ -53,73 +66,202 @@ module Fastlane
       end
 
       def self.create_xcframework(params)
-        xcframework = @xchelper.xcframework_path
-        begin
-          FileUtils.rm_rf(xcframework) if File.exist?(xcframework)
+        UI.message("params: #{params}")
 
-          arguments = ['-create-xcframework']
-          arguments << '-allow-internal-distribution' if params[:allow_internal_distribution]
-          params[:destinations].each_with_index do |_, index|
-            arguments << "-framework #{@xchelper.xcarchive_framework_path(index)}"
-            arguments << debug_symbols(index: index, params: params)
+        if params[:frameworks]
+          params[:frameworks].each do |framework|
+            UI.message("▸ Creating xcframework for #{framework}")
+            xcframework = @xchelper.get_xcframework_path(framework)
+            UI.message("▸ Creating xcframework for #{framework} at path: #{xcframework}")
+            begin
+              FileUtils.rm_rf(xcframework) if File.exist?(xcframework)
+      
+              arguments = ['-create-xcframework']
+              arguments << '-allow-internal-distribution' if params[:allow_internal_distribution]
+      
+              params[:destinations].each_with_index do |_, index|
+                arguments << "-framework #{@xchelper.get_xcarchive_framework_path(index, framework)}"
+                arguments << debug_symbols(index: index, params: params, framework: framework)
+              end
+              arguments << "-output #{xcframework}"
+              UI.message("set -o pipefail && xcodebuild #{arguments.reject(&:empty?).join(' ')}")
+
+              Actions.sh("set -o pipefail && xcodebuild #{arguments.reject(&:empty?).join(' ')}")
+            rescue StandardError => e
+              UI.user_error!(e)
+            end
           end
-          arguments << "-output #{xcframework}"
-
-          Actions.sh("set -o pipefail && xcodebuild #{arguments.reject(&:empty?).join(' ')}")
-        rescue StandardError => e
-          UI.user_error!(e)
+        else
+          xcframework = @xchelper.xcframework_path
+          UI.message("▸ Creating xcframework at path: #{xcframework}")
+          begin
+            FileUtils.rm_rf(xcframework) if File.exist?(xcframework)
+      
+            arguments = ['-create-xcframework']
+            arguments << '-allow-internal-distribution' if params[:allow_internal_distribution]
+      
+            params[:destinations].each_with_index do |_, index|
+              arguments << "-framework #{@xchelper.xcarchive_framework_path(index)}"
+              arguments << debug_symbols(index: index, params: params, framework: @xchelper.framework)
+            end
+            arguments << "-output #{xcframework}"
+      
+            Actions.sh("set -o pipefail && xcodebuild #{arguments.reject(&:empty?).join(' ')}")
+          rescue StandardError => e
+            UI.user_error!(e)
+          end
         end
       end
 
-      def self.debug_symbols(index:, params:)
-        return '' if !Helper.xcode_at_least?('12.0.0') || params[:include_debug_symbols] == false
-
-        debug_symbols = []
-
-        # Include dSYMs in xcframework
-        if params[:include_dSYMs] != false
-          debug_symbols << "-debug-symbols #{@xchelper.xcarchive_dSYMs_path(index)}/#{@xchelper.framework}.dSYM"
-        end
-
-        # Include BCSymbols in xcframework
-        if params[:include_BCSymbolMaps] != false && params[:enable_bitcode] != false
-          bc_symbols_dir = @xchelper.xcarchive_BCSymbolMaps_path(index)
-          if Dir.exist?(bc_symbols_dir)
-            arguments = Dir.children(bc_symbols_dir).map { |path| "-debug-symbols #{File.expand_path("#{bc_symbols_dir}/#{path}")}" }
-            debug_symbols << arguments.join(' ')
+      def self.sign_xcframework(params)
+        return if params[:code_sign_identity].nil?
+      
+        frameworks = params[:frameworks] || [nil]
+      
+        frameworks.each do |framework|
+          xcframework = framework ? @xchelper.get_xcframework_path(framework) : @xchelper.xcframework_path
+          UI.message("▸ Signing xcframework with '#{params[:code_sign_identity]}' at path: #{xcframework}")
+      
+          begin
+            command = "codesign --force --sign '#{params[:code_sign_identity]}' --timestamp=none #{xcframework}"
+            UI.message(command)
+            Actions.sh(command)
+          rescue StandardError => e
+            UI.user_error!(e)
           end
         end
+      end
 
+      def self.zip_xcframework(params)
+        return if params[:zip_xcframework].nil?
+
+        # Check if the zip utility is installed
+        unless system("which zip > /dev/null 2>&1")
+          UI.important("zip utility is not installed. Installing...")
+          system("brew install zip") || UI.crash!("Error: Failed to install zip utility")
+        end
+
+        frameworks = params[:frameworks] || [nil]
+
+        frameworks.each do |framework|
+          xcframework = framework ? @xchelper.get_xcframework_path(framework) : @xchelper.xcframework_path
+          UI.message("▸ Zipping xcframework at path: #{xcframework}")
+
+          begin
+            zip_path = "#{xcframework}.zip"
+            FileUtils.rm_f(zip_path) if File.exist?(zip_path)
+
+            command = "zip -r -X #{zip_path} #{xcframework}"
+            UI.message(command)
+
+            Actions.sh(command)
+          rescue StandardError => e
+            UI.user_error!(e)
+          end
+        end
+      end
+
+      def self.delete_xcframework(params)
+        return if params[:delete_xcframework].nil?
+
+        frameworks = params[:frameworks] || [nil]
+
+        frameworks.each do |framework|
+          xcframework = framework ? @xchelper.get_xcframework_path(framework) : @xchelper.xcframework_path
+          UI.message("▸ Deleting xcframework at path: #{xcframework}")
+
+          begin
+            FileUtils.rm_rf(xcframework)
+          rescue StandardError => e
+            UI.user_error!(e)
+          end
+        end
+      end
+
+
+
+      # #Fix compile problem go to xcframework and run this command (https://developer.apple.com/forums/thread/123253):
+      # #The generated file includes many module references that Swift thinks are class references because it uses the class ahead of the module
+      # #The problem is that in the Swiftinterface file, we have a class named ABCConnections, but the module is also called ABCConnections. 
+      def self.remove_module_reference(params)
+        return if params[:ignore_module_reference].nil?
+      
+        frameworks = params[:frameworks] || [nil]
+      
+        frameworks.each do |framework|
+          output_path = framework ? @xchelper.get_xcframework_path(framework) : @xchelper.xcframework_path
+          params[:ignore_module_reference].each do |module_reference|
+            begin
+              command = "find #{output_path} -name '*.swiftinterface' -exec sed -i -e 's/#{module_reference}\\.//g' {} \\;"
+              success = system(command)
+              unless success
+                UI.error("Failed to execute command: #{command}")
+              else
+                UI.success("▸ Removed module reference for #{module_reference} in all matching files")
+              end
+            rescue => e
+              UI.error("Error: #{e.message}")
+            end
+          end
+        end
+      end
+
+      def self.debug_symbols(index:, params:, framework: nil)
+        return '' if !Helper.xcode_at_least?('12.0.0') || params[:include_debug_symbols] == false
+      
+        frameworks = params[:frameworks] || [framework]
+        debug_symbols = []
+      
+        frameworks.each do |fw|
+          # Include dSYMs in xcframework
+          if params[:include_dSYMs] != false
+            debug_symbols << "-debug-symbols #{@xchelper.xcarchive_dSYMs_path(index)}/#{fw}.dSYM"
+          end
+      
+          # Include BCSymbols in xcframework
+          if params[:include_BCSymbolMaps] != false
+            bc_symbols_dir = @xchelper.xcarchive_BCSymbolMaps_path(index)
+            if Dir.exist?(bc_symbols_dir)
+              arguments = Dir.children(bc_symbols_dir).map { |path| "-debug-symbols #{File.expand_path("#{bc_symbols_dir}/#{path}")}" }
+              debug_symbols << arguments.join(' ')
+            end
+          end
+        end
+      
         debug_symbols.join(' ')
       end
 
       def self.copy_dSYMs(params)
         return if params[:include_dSYMs] == false
-
-        dSYMs_output_dir = @xchelper.xcframework_dSYMs_path
-        FileUtils.mkdir_p(dSYMs_output_dir)
-
-        params[:destinations].each_with_index do |_, framework_index|
-          dSYM_source = "#{@xchelper.xcarchive_dSYMs_path(framework_index)}/#{@xchelper.framework}.dSYM"
+      
+        frameworks = params[:frameworks] || [nil]
+      
+        frameworks.each_with_index do |framework, framework_index|
+          dSYMs_output_dir = framework ? @xchelper.framework_dSYMs_path(framework) : @xchelper.xcframework_dSYMs_path
+          FileUtils.mkdir_p(dSYMs_output_dir)
+      
+          dSYM_source = "#{@xchelper.xcarchive_dSYMs_path(framework_index)}/#{framework}.dSYM"
           identifier = @xchelper.library_identifier(framework_index)
-          dSYM = "#{@xchelper.framework}.#{identifier}.dSYM"
+          dSYM = "#{framework}.#{identifier}.dSYM"
           dSYM_destination = "#{dSYMs_output_dir}/#{dSYM}"
-
+      
           UI.important("▸ Copying #{dSYM} to #{dSYMs_output_dir}")
           FileUtils.cp_r(dSYM_source, dSYM_destination)
         end
       end
 
       def self.copy_BCSymbolMaps(params)
-        return if params[:enable_bitcode] == false || params[:include_BCSymbolMaps] == false
-
-        symbols_output_dir = @xchelper.xcframework_BCSymbolMaps_path
-        FileUtils.mkdir_p(symbols_output_dir)
-
-        params[:destinations].each_with_index do |_, framework_index|
+        return if params[:include_BCSymbolMaps] == false
+      
+        frameworks = params[:frameworks] || [nil]
+      
+        frameworks.each_with_index do |framework, framework_index|
+          symbols_output_dir = framework ? @xchelper.framework_BCSymbolMaps_path(framework) : @xchelper.xcframework_BCSymbolMaps_path
+          FileUtils.mkdir_p(symbols_output_dir)
+      
           symbols_xcarchive_dir = @xchelper.xcarchive_BCSymbolMaps_path(framework_index)
           next unless Dir.exist?(symbols_xcarchive_dir)
-
+      
           FileUtils.cp_r("#{symbols_xcarchive_dir}/.", symbols_output_dir)
           UI.important("▸ Copying #{Dir.children(symbols_xcarchive_dir)} to #{symbols_output_dir}")
         end
@@ -133,22 +275,32 @@ module Fastlane
       end
 
       def self.update_xcargs(params)
-        xcargs = []
-        if params[:override_xcargs]
-          UI.important('Overwriting SKIP_INSTALL and BUILD_LIBRARY_FOR_DISTRIBUTION options')
-          if params[:xcargs]
-            params[:xcargs].gsub!(/SKIP_INSTALL(=|\s+)(YES|NO)/, '')
-            params[:xcargs].gsub!(/BUILD_LIBRARY_FOR_DISTRIBUTION(=|\s+)(YES|NO)/, '')
+        xcargs = params[:override_xcargs].to_s.strip.split(' ')
+        skip_install_set = false
+        build_library_for_distribution_set = false
+      
+        xcargs.each do |arg|
+          if arg.match?(/SKIP_INSTALL(=|\s+)/)
+            skip_install_set = true
+            unless arg.match?(/SKIP_INSTALL=NO/)
+              xcargs.delete(arg)
+              xcargs << 'SKIP_INSTALL=NO'
+            end
           end
-          xcargs.concat(['SKIP_INSTALL=NO', 'BUILD_LIBRARY_FOR_DISTRIBUTION=YES'])
+      
+          if arg.match?(/BUILD_LIBRARY_FOR_DISTRIBUTION(=|\s+)/)
+            build_library_for_distribution_set = true
+            unless arg.match?(/BUILD_LIBRARY_FOR_DISTRIBUTION=YES/)
+              xcargs.delete(arg)
+              xcargs << 'BUILD_LIBRARY_FOR_DISTRIBUTION=YES'
+            end
+          end
         end
-
-        if params[:enable_bitcode] != false
-          params[:xcargs].gsub!(/ENABLE_BITCODE(=|\s+)(YES|NO)/, '') if params[:xcargs]
-          xcargs << ['OTHER_CFLAGS="-fembed-bitcode"', 'BITCODE_GENERATION_MODE="bitcode"', 'ENABLE_BITCODE=YES']
-        end
-
-        "#{params[:xcargs].to_s.strip} #{xcargs.join(' ')}"
+      
+        xcargs << 'SKIP_INSTALL=NO' unless skip_install_set
+        xcargs << 'BUILD_LIBRARY_FOR_DISTRIBUTION=YES' unless build_library_for_distribution_set
+      
+        xcargs.join(' ')
       end
 
       def self.update_destinations(params)
@@ -219,13 +371,6 @@ module Fastlane
             optional: false
           ),
           FastlaneCore::ConfigItem.new(
-            key: :enable_bitcode,
-            description: 'Should the project be built with bitcode enabled?',
-            optional: true,
-            is_string: false,
-            default_value: true
-          ),
-          FastlaneCore::ConfigItem.new(
             key: :destinations,
             description: 'Use custom destinations for building the xcframework',
             optional: true,
@@ -282,6 +427,37 @@ module Fastlane
                           'Set this to false to preserve the passed xcargs',
             optional: true,
             default_value: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :frameworks,
+            description: 'List of frameworks to create xcframework for',
+            optional: true,
+            is_string: false,
+            type: Array,
+            default_value: []),
+          FastlaneCore::ConfigItem.new(
+            key: :code_sign_identity,
+            description: 'Code sign identity to use for building the xcframework',
+            optional: true
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :zip_xcframework,
+            description: 'Zip the xcframework after creation',
+            optional: true,
+            default_value: false
+          ),
+          FastlaneCore::ConfigItem.new(
+            key: :ignore_module_reference,
+            description: 'List of module references to remove from the xcframework',
+            optional: true,
+            is_string: false,
+            type: Array,
+            default_value: []),
+          FastlaneCore::ConfigItem.new(
+            key: :delete_xcframework,
+            description: 'Delete the xcframework after creation',
+            optional: true,
+            default_value: false
           )
         ]
       end
